c40f10708d129d26ef51587bc84b5acd43c3d6ff
graceleung <graceleungyanyee@gmail.com>
2025-04-19 03:06:04 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello, Upwork!</h1>
+      <p>這是我第一個 Next.js 專案！</p>
+    </div>
+  );
+}
